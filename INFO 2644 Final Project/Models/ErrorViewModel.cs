6b776e8f412d350be9886584cc7efda10d4d5de6
dk545617 <dk545617@users.noreply.github.com>
2020-12-13 12:47:01 -0600

    Add project files.

---
+++ INFO 2644 Final Project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace INFO_2644_Final_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
